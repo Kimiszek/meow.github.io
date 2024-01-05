dfe99a4e116a5b02e93acc602d109eda2f8359af
Kimiszek <155777138+Kimiszek@users.noreply.github.com>
2024-01-05 22:22:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ yesBtn.addEventListener("click", () => {
     "https://media.giphy.com/media/VKUSdDIW7vXWwTFCtf/giphy.gif";
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+nieBtn.addEventListener("mouseover", () => {
+  const nieBtnRect = nieBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - nieBtnRect.width;
+  const maxY = window.innerHeight - nieBtnRect.height;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+  nieBtn.style.left = randomX + "px";
+  nieBtn.style.top = randomY + "px";
 });
